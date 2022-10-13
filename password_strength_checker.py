0db9404e580aa6590285192599e9101efd96bcd1
pardhu9999 <104009257+pardhu9999@users.noreply.github.com>
2022-10-13 21:06:48 +0530

    Add files via upload

---
+++ password_strength_checker.py
@@ -0,0 +1,11 @@
+#########################PASSWORD STRENGTH CHECKER########################
+import re
+print("Password Strength checker")
+v=input("Enter the password:")
+if(len(v)>=8):
+    if(bool(re.match('((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,30})',v))==True):
+        print("The password is strong")
+    elif(bool(re.match('((\d*)([a-z]*)([A-Z]*)([!@#$%^&*]*).{8,30})',v))==True):
+        print("The password is weak")
+else:
+    print("You have entered an invalid password.")
